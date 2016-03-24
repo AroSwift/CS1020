e7d7aa20e4efdf9658eed3bfc818064d3d0cd457
AroSwift <abarlow505@gmail.com>
2016-03-23 20:14:12 -0400

    Stable state 0.1

--- Tree Traversal/Tree Traversal/Tree Traversal/traversal.cpp
+++ Tree Traversal/Tree Traversal/Tree Traversal/traversal.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int main() {
     vector<Person> people;
-    
-    
+
+
 }
 
 
@@ -17,55 +17,43 @@ void load_people(vector<Person> people) {
     ifstream input;
     char filename[MAX_FILE_LENGTH];
     bool file_errors;
-    
+
     do { // Find a file that exists
         file_errors = false;
-        
+
         // Prompt for file and read it in
         cout << "Enter file name: ";
         cin.getline( filename, MAX_FILE_LENGTH );
-        
+
         input.open( filename );
-        
+
         // When file could not be found
         if( input.fail() ) {
             cout << "Input file " << filename << " does not exist. \n";
             file_errors = true;
-            
+
             // When file is empty
         } else if( input.peek() == EOF ) {
             cout << "Input file " << filename << " is empty. \n";
             file_errors = true;
         }
-        
+
     } while( file_errors );
 
-    
-    input.open(FILE_NAME);
-    
-    // When file could not be found
-    if( input.fail() ) {
-        cout << "Input file " << FILE_NAME << " does not exist." << endl;
-        exit(1);
+
+    // Read in file row by row
+    for( int i = 0; i < people.size(); i++ ) {
+        // Ensure null character is accounted for in file
+        string first_name, last_name, street_address, city, state, zip;
+        getline( input, first_name, ',' );
+        getline( input, last_name, ',' );
+        getline( input, street_address, ',' );
+        getline( input, city, ',' );
+        getline( input, state, ',' );
+        getline( input, zip, ',' );
         
-        // When file is empty
-    } else if( input.peek() == EOF ) {
-        cout << "Input file " << FILE_NAME << " is empty." << endl;
-        exit(1);
+        people.push_back( Person(first_name, last_name, street_address, city, state, zip) );
+
     }
-    
-    // Set previous node to point to first
-    Contact *prev_node = *first;
-    
-    while( !input.eof() ) {
-        string first_name, last_name, phone_number;
-        input >> first_name;
-        input >> last_name;
-        input >> phone_number;
-        
-    }
-    
-    // Close file
-    input.close();
-    
+
 }

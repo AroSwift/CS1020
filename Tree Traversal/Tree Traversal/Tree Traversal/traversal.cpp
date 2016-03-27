04921ad4065287c5bf5a2e8afbbbf4f35f82de27
AroSwift <abarlow505@gmail.com>
2016-03-27 00:21:38 -0400

    Custom fields selection menus

--- Tree Traversal/Tree Traversal/Tree Traversal/traversal.cpp
+++ Tree Traversal/Tree Traversal/Tree Traversal/traversal.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 int main() {
    Tree<Person>* tree = new Tree<Person>;
+   Settings *s = new Settings();
 
    load_people(tree);
 
    // Show the menu
-   main_menu(tree);
-   
+   main_menu(tree, s);
+
    delete tree;
 
 }
@@ -74,7 +75,7 @@ void load_people(Tree<Person>* tree) {
 //
 // main_menu
 //
-void main_menu(Tree<Person>* tree) {
+void main_menu(Tree<Person>* tree, Settings *s) {
    bool exit = false;
    char choice;
 
@@ -97,26 +98,31 @@ void main_menu(Tree<Person>* tree) {
       switch(choice) {
          case '1': // Pre-order traversal
             cout << endl;
+            s->options_menu();
             tree->pre_order_traversal();
             break;
 
          case '2': // In-order traversal
             cout << endl;
+            s->options_menu();
             tree->in_order_traversal();
             break;
 
          case '3': // Post-order traversal
             cout << endl;
+            s->options_menu();
             tree->post_order_traversal();
             break;
 
          case '4': // Breadth-order traversal
             cout << endl;
+            s->options_menu();
             tree->breadth_first_traversal();
             break;
 
          case '5': // Search for Name
             cout << endl;
+            s->options_menu();
             search_for_person();
             break;
 
@@ -133,62 +139,98 @@ void main_menu(Tree<Person>* tree) {
    } while(!exit);
 }
 
-void options_menu(Tree<Person>* tree) {
+void Settings::options_menu() {
+   if( prior_settings ) {
+      bool acceptable_input;
+      do {
+         char user_choice;
+         cout << "Would you like to use your previous settings? (Y, N): ";
+         cin >> user_choice;
+
+         if( user_choice == 'Y' || user_choice == 'y' ) {
+            acceptable_input = true;
+            change_settings_menu();
+         } else if( user_choice == 'N' || user_choice == 'n' ) {
+            acceptable_input = true;
+         } else {
+            cout << "Please enter 'Y' or 'N' to indicate whether you would like to use the prior settings" << endl;
+            acceptable_input = false;
+         }
+      } while( !acceptable_input );
+   } else {
+      change_settings_menu();
+   }
+}
+
+void Settings::change_settings_menu() {
    bool exit = false;
    char choice;
-   
+
    // Display a menu
    do {
-      
+
       // Give user choices
-      cout << "Options Menu" << endl
-      << "------------------" << endl
-      << "1.) Pre-order traversal" << endl
-      << "2.) In-order traversal" << endl
-      << "3.) Post-order traversal" << endl
-      << "4.) Breadth-first traversal" << endl
-      << "5.) Search for a name" << endl
-      << "6.) Exit" << endl
+      cout << "Field Selection Menu" << endl
+      << "-------------------------" << endl
+      << "Please select which fields you would like to be displayed:" << endl
+      << "1.) Show first name" << endl
+      << "2.) Show last name" << endl
+      << "3.) Show street address" << endl
+      << "4.) Show city" << endl
+      << "5.) Show state" << endl
+      << "6.) Show zip" << endl
+      << "7.) Exit field selection" << endl
       << "Choice: ";
       cin >> choice;
-      
+
       // Associate choice with an action
       switch(choice) {
          case '1': // Pre-order traversal
             cout << endl;
-            tree->pre_order_traversal();
+            can_show_first_name = true;
+            prior_settings = true;
             break;
-            
+
          case '2': // In-order traversal
             cout << endl;
-            tree->in_order_traversal();
+            can_show_last_name = true;
+            prior_settings = true;
             break;
-            
+
          case '3': // Post-order traversal
             cout << endl;
-            tree->post_order_traversal();
+            can_show_street_address = true;
+            prior_settings = true;
             break;
-            
+
          case '4': // Breadth-order traversal
             cout << endl;
-            tree->breadth_first_traversal();
+            can_show_city = true;
+            prior_settings = true;
             break;
-            
+
          case '5': // Search for Name
             cout << endl;
-            search_for_person();
+            can_show_state = true;
+            prior_settings = true;
             break;
-            
-         case '6': // Exit program
+
+         case '6': // Search for Name
+            cout << endl;
+            can_show_zip = true;
+            prior_settings = true;
+            break;
+
+         case '7': // Exit program
             exit = true;
             break;
-            
+
          default: // Error occured
             cout << "Please enter a valid option." << endl;
             cout << endl;
             break;
       }
-      
+
    } while(!exit);
 }
 
--- Tree Traversal/Tree Traversal/Tree Traversal/traversal.h
+++ Tree Traversal/Tree Traversal/Tree Traversal/traversal.h
@@ -5,29 +5,15 @@ using namespace std;
 
 const int MAX_FILE_LENGTH = 256; // Max file length on linux is 255
 
-class Person {
-   string first_name;
-   string last_name;
-   string street_address;
-   string city;
-   string state;
-   string zip;
-public:
-   Person();
-   Person( string first_name, string last_name, string street_address, string city, string state, string zip );
-//   ~Person();
-   void print_person_data();
-   void search_for_person();
-   friend ostream& operator<<(ostream& os, Person& p);
-//   friend bool operator<(const Person& left, const Person& right);
-   friend bool compare_people(Person p1, Person p2);
-};
-
 class Settings {
 public:
    Settings();
-   ~Settings();
+   //   ~Settings();
+   void set_settings( bool show_first_name, bool show_last_name, bool show_street_address, bool show_city, bool show_state, bool show_zip );
+   void options_menu();
+   void change_settings_menu();
 private:
+   bool prior_settings;
    bool can_show_first_name;
    bool can_show_last_name;
    bool can_show_street_address;
@@ -36,6 +22,49 @@ private:
    bool can_show_zip;
 };
 
+
+class Person {
+public:
+   Person() {};
+   Person( string first_name, string last_name, string street_address, string city, string state, string zip );
+//   ~Person();
+   void print_person_data();
+   void search_for_person();
+   friend ostream& operator<<(ostream& os, Person& p);
+   friend bool compare_people(Person p1, Person p2);
+private:
+   string first_name;
+   string last_name;
+   string street_address;
+   string city;
+   string state;
+   string zip;
+};
+
+Settings::Settings() {
+    prior_settings = false;
+    can_show_first_name = true;
+    can_show_last_name = true;
+    can_show_street_address = true;
+    can_show_city = true;
+    can_show_state = true;
+    can_show_zip = true;
+}
+
+//Settings::~Settings() {
+//   delete this;
+//}
+
+void Settings::set_settings(bool show_first_name, bool show_last_name, bool show_street_address, bool show_city, bool show_state, bool show_zip) {
+    this->prior_settings = true;
+    this->can_show_first_name = show_first_name;
+    this->can_show_last_name = show_last_name;
+    this->can_show_street_address = show_street_address;
+    this->can_show_city = show_city;
+    this->can_show_state = show_state;
+    this->can_show_zip = show_zip;
+}
+
 Person::Person( string first_name, string last_name, string street_address, string city, string state, string zip ) {
    this->first_name = first_name;
    this->last_name = last_name;
@@ -43,9 +72,6 @@ Person::Person( string first_name, string last_name, string street_address, stri
    this->city = city;
    this->state = state;
    this->zip = zip;
-}
-
-Person::Person() {
 }
 
 // Person::~Person() {
@@ -60,6 +86,7 @@ bool compare_people(Person p1, Person p2) {
 
 ostream& operator<<(ostream& os, Person& p) {
    os << p.first_name << ", " << p.last_name;
+//   if( can_show_first_name ) os << p.first_name;
    return os;
 }
 
@@ -67,10 +94,11 @@ ostream& operator<<(ostream& os, Person& p) {
 //   return((left.last_name < right.last_name) || ((left.last_name == right.last_name) && (left.first_name < right.first_name)));
 //}
 
-// Menu Options
 void load_people(Tree<Person>* tree);
-void main_menu(Tree<Person>* tree);
-void options_menu(Tree<Person>* tree);
+
+// Menu Options
+void main_menu(Tree<Person>* tree, Settings *s);
+void options_menu(Tree<Person>* tree, Settings *s);
 
 void search_for_person();
 
